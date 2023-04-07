26a298edc442a2589d5165344cf25b2a1d5e0c7a
Kacique <wbrand90@gmail.com>
2023-04-07 12:44:47 -0700

    Completed some of the functionality of the game

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,12 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    var option = ["🪨", "📃", "✂️"]
+    
+    @State var correctChoice = Int.random(in: 0...2)
+    @State var shouldWin = Bool.random()
+    @State var message = ""
+    @State var playerScore = 0
+    
+    
+    
+    func playRound() -> String{
+        
+        var roundMessage = message
+        
+        if shouldWin{
+            roundMessage = "Win"
+            
+        } else {
+            roundMessage = "Lose"
+        }
+        
+        return roundMessage
+    }
+    
+    func optionTapped(_ number: Int) -> Int {
+        print("\(option[number])")
+        if option[correctChoice] == "🪨" && shouldWin == true{
+            switch option{
+            case ["📃"]:
+                playerScore += 1
+                print("Win")
+            case ["✂️"]:
+                playerScore -= 1
+                print("Lose")
+            default:
+                print("Tie")
+            }
+        }
+        shouldWin.toggle()
+        
+        return number
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Player Score: \(playerScore)")
+            Text("\(option[correctChoice])")
+                .font(.system(size: 200))
+            Text("\(playRound())")
+            
+            HStack{
+                ForEach(0..<3) { number in
+                    Button {
+                        optionTapped(number)
+                    } label: {
+                        Text(option[number])
+                            .clipShape(Capsule())
+                            .shadow(radius: 5)
+                    }
+                }
+            }
+            
+    
         }
         .padding()
     }
